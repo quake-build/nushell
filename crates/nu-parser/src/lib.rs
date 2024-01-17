f46dc74e4e0ab23252c892ea615a82d964548405
cassaundra <cass@cassaundra.org>
2024-01-17 06:28:19 -0800

    nu-parser: expose all parser internals

--- crates/nu-parser/src/lib.rs
+++ crates/nu-parser/src/lib.rs
@@ -20,10 +20,7 @@ pub use lite_parser::{lite_parse, LiteBlock, LiteElement};
 pub use parse_keywords::*;
 pub use parser_path::*;
 
-pub use parser::{
-    is_math_expression_like, parse, parse_block, parse_expression, parse_external_call,
-    parse_unit_value, trim_quotes, trim_quotes_str, unescape_unquote_string, DURATION_UNIT_GROUPS,
-};
+pub use parser::*;
 
 #[cfg(feature = "plugin")]
 pub use parse_keywords::parse_register;
