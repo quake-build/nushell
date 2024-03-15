8bca91da9025d7aea9e394721b442283a0e7568c
cassaundra <cass@cassaundra.org>
2024-03-15 03:04:12 -0700

    Expose `parse_internal_call` in parser

--- crates/nu-parser/src/lib.rs
+++ crates/nu-parser/src/lib.rs
@@ -23,7 +23,8 @@ pub use parser_path::*;
 
 pub use parser::{
     is_math_expression_like, parse, parse_block, parse_expression, parse_external_call,
-    parse_unit_value, trim_quotes, trim_quotes_str, unescape_unquote_string, DURATION_UNIT_GROUPS,
+    parse_internal_call, parse_unit_value, trim_quotes, trim_quotes_str, unescape_unquote_string,
+    DURATION_UNIT_GROUPS,
 };
 
 #[cfg(feature = "plugin")]
